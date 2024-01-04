314ee7188ea872ac7dab6560fc750051dca4b5c1
Sheeba0511 <sn5901280@gmail.com>
2024-01-04 22:16:29 +0530

    add file

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,40 +1,99 @@
 import React, { useEffect, useState } from "react";
+import { Button, Form, Modal } from "react-bootstrap";
 
 export default function Admin() {
-  const [product, setProduct] = useState([]);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [products, setProducts] = useState([]);
   useEffect(() => {
     fetch("https://dummyjson.com/products")
       .then((res) => res.json())
       .then((data) => {
-        // setProduct(data.allProducts);
-        console.log(product);
+        setProducts(data.products);
+        console.log(products);
       });
   }, []);
+
   return (
     <>
-      <h1> Admin Page</h1>
+      <h5 className="admin_text"> Admin Page</h5>
       <div className="table_admin">
-        <table>
+        <table striped bordered hover>
           <thead>
             <tr>
-              <th>Title</th>
-              <th>image</th>
-              <th>Price</th>
+              <th>Product Name</th>
+              <th>Product Image</th>
+              <th>Product Price</th>
               <th>Rating</th>
               <th>Action</th>
             </tr>
           </thead>
           <tbody>
-            {product.map((cv, idx) => {
+            {products.map((cv, idx) => {
               return (
                 <tr key={idx}>
-                  <td>{cv.title}</td>
-                  <td>{cv.img}</td>
-                  <td>{cv.price}</td>
-                  <td>{cv.rating}</td>
                   <td>
-                    <button className="btn btn-primary">Edit</button>
-                    <button className="btn btn-danger">Delete</button>
+                    <p>{cv.title}</p>
+                  </td>
+                  <td>
+                    <div className="img_products2">
+                      {" "}
+                      <img src={cv.images[0]} alt="Product" />
+                    </div>
+                  </td>
+                  <td>
+                    <p>{cv.price}</p>
+                  </td>
+                  <td>
+                    <p>{cv.rating}</p>
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-primary btn-sm me-1"
+                      onClick={handleShow}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      type="button"
+                      className="btn-close"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                    ></button>
+                    <Modal show={show} onHide={handleClose}>
+                      <Modal.Header closeButton>
+                        <Modal.Title>Modal Products</Modal.Title>
+                      </Modal.Header>
+
+                      <Modal.Body>
+                        <Form.Label>Product Name</Form.Label>
+                        <Form.Control
+                          type="text"
+                          placeholder="Name of product"
+                        />
+                        <Form.Label>Price</Form.Label>
+                        <Form.Control
+                          type="text"
+                          placeholder="Price of product"
+                        />
+                        <Form.Label>Rating</Form.Label>
+                        <Form.Control
+                          type="text"
+                          placeholder="Rating of product"
+                        />
+                      </Modal.Body>
+
+                      <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                          Close
+                        </Button>
+                        <Button variant="primary" onClick={handleClose}>
+                          Save Changes
+                        </Button>
+                      </Modal.Footer>
+                    </Modal>
                   </td>
                 </tr>
               );
--- src/pages/RequireAuth.js
+++ src/pages/RequireAuth.js
@@ -1,16 +1,11 @@
 import React from "react";
 import { useCookies } from "react-cookie";
-import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 export default function RequireAuth() {
   const [cookies, setCookie] = useCookies(["token"]);
   const auth = cookies.token;
   console.log({ auth: auth });
-  const location = useLocation();
-  return auth ? (
-    <Outlet />
-  ) : (
-    // <Navigate to="/login" state={{ from: location }} replace />
-    <Navigate to="/" />
-  );
+
+  return auth ? <Outlet /> : <Navigate to="/" />;
 }
